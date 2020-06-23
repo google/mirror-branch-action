a18246e42581c2c108c884ea6f19c13d0d1aaee1
Jeffrey Yasskin <jyasskin@google.com>
2020-06-23 15:52:53 -0700

    Log some more information.

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-async function run() {
+async function run(): Promise<void> {
   try {
     const githubToken = core.getInput('github-token');
     const sourceBranchName = core.getInput('source');
     const destBranchName = core.getInput('dest');
-    console.log(`Mirroring ${sourceBranchName} to ${destBranchName}.`);
 
     const octokit = github.getOctokit(githubToken);
     const sourceBranchTip = await octokit.git.getRef({
@@ -14,11 +13,21 @@ async function run() {
       ref: `heads/${sourceBranchName}`,
     });
 
-    await octokit.git.updateRef({
+    if (sourceBranchTip.data.object.type !== "commit") {
+      throw new Error(`Expected branch ${sourceBranchName} to resolve to a commit. Got a ${sourceBranchTip.data.object.type}.`);
+    }
+
+    const sourceBranchSha = sourceBranchTip.data.object.sha;
+
+    console.log(`Pushing ${sourceBranchName} (${sourceBranchSha}) to ${destBranchName}.`);
+
+    const result = await octokit.git.updateRef({
       ...github.context.repo,
       ref: `heads/${destBranchName}`,
-      sha: sourceBranchTip.data.object.sha,
+      sha: sourceBranchSha,
     })
+
+    console.log(`Set ${result.data.ref} to ${result.data.object.sha}.`);
   } catch (error) {
     core.setFailed(error.message);
   }
